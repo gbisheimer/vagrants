957f1ded3ae320ca692282a9dd4a456c03bea01a
Guillermo Bisheimer <gbisheimer@bys-control.com.ar>
2014-06-29 16:27:00 -0300

    puppet file

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,3 +3,41 @@ package { "git": ensure => present }
 package { "mc": ensure => present }
 package { "openssl": ensure => present }
 package { "shellinabox": ensure => present }
+
+# Installs PostgreSQL 9.3 server from PGDG repository
+class {'postgresql::globals':
+  version => '9.3',
+  manage_package_repo => true,
+  encoding => 'UTF8',
+  locale  => 'it_IT.utf8',
+}->
+class { 'postgresql::server':
+  ensure => 'present',
+  ip_mask_deny_postgres_user => '0.0.0.0/32', # Allows posgres user connection from any host
+  ip_mask_allow_all_users    => '0.0.0.0/0',
+  listen_addresses => '*',
+  postgres_password => 'postgres',            # Sets default postgres user password
+}
+
+# Installs contrib modules
+class { 'postgresql::server::contrib':
+  package_ensure => 'present',
+}
+
+#****************************************************************
+# Vagrant
+#****************************************************************postgresql::server::role { 'vagrant':
+  password_hash => postgresql_password('vagrant', 'vagrant'),
+  createdb => true,
+  login => true,
+  replication => true,
+}
+
+postgresql::server::pg_hba_rule { 'vagrant':
+  description => "Allows md5 authentication to user vagrant",
+  type => 'host',
+  address => '0.0.0.0/0',
+  database => 'all',
+  user => 'vagrant',
+  auth_method => 'md5',
+}
